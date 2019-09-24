79f769109abcb6fabd3910f67d13a205a5bcca8b
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-09-24 12:28:30 -0400

    get bytecode and generate compiled object

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path  = require('path');
 const { exec } = require('child_process');
+const async = require('async');
 
 function buf2hex(buffer) { // buffer is an ArrayBuffer
   return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
@@ -15,24 +16,39 @@ class NimCompiler {
   }
 
   async compile(contractFiles, options, cb) {
-    // Compile file
+    const compiledObject = {};
 
-    // Get code from wasm file
-    // const codeHex = "0x" + buf2hex(fs.readFileSync(wasmFile));
+    async.each(contractFiles, (file, eachCb) => {
+      const className = path.basename(file.path).split('.')[0];
+      compiledObject[className] = {};
+
+      // Compile file
+      // TODO wait for docker image
 
 
-    // Get ABI from nim file
-    exec(`${path.join(__dirname, './tools/abi_gen')} ${contractFiles[0].path}`, (err, stdout, stderr) => {
-      if (err) {
-        this.logger.error('Error while getting ABI');
-        this.logger.error(stderr);
-	return cb(err);
-     }
-     try {
-       const abi = JSON.parse(stdout);
-     } catch(e) {
-       return cb(e);
-     }
+      // Get code from wasm file
+      const codeHex = "0x" + buf2hex(fs.readFileSync(file.path.replace('.nim', '.wasm')));
+
+      compiledObject[className].runtimeBytecode = codeHex;
+      compiledObject[className].realRuntimeBytecode = codeHex;
+      compiledObject[className].code = codeHex;
+
+      // Get ABI from nim file
+      exec(`${path.join(__dirname, './tools/abi_gen')} ${file.path}`, (err, stdout, stderr) => {
+        if (err) {
+          this.logger.error('Error while getting ABI');
+          this.logger.error(stderr);
+  	  return eachCb(err);
+       }
+       try {
+         const abi = JSON.parse(stdout);
+         compiledObject[className].abiDefinition = abi;
+         return eachCb();
+       } catch(e) {
+         return eachCb(e);
+       }
+    }, (err) => {
+      cb(err, compiledObject);
     });
   }
 }
